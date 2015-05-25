98afeffd3ddbc4990ebb968f0d6b417c6e829fdf
Chris Watts <cwatts@simplymeasured.com>
2015-05-25 09:25:03 -0700

    Added fixtures for users and content Added content route

---
+++ 2015-05-28-ember-cli-mirage/code/app/mirage/fixtures/users.js
@@ -0,0 +1,5 @@
+export default [{
+  name: 'Tom Dale'
+}, {
+  name: 'Chris'
+}];
--- 2015-05-28-ember-cli-mirage/code/app/routes/login.js
+++ 2015-05-28-ember-cli-mirage/code/app/routes/login.js
@@ -1,4 +1,9 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  actions: {
+    login: function() {
+      Ember.debug('Logging in');
+    }
+  }
 });
